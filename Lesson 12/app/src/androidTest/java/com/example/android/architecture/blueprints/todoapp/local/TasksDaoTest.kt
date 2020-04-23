a4c0575d60fffcce9507030c757bc2b8b92246f4
Joao Barbosa <joao.barbosa@jci.com>
2020-04-23 11:23:14 +0100

    Added Lesson 12 solution

---
+++ Lesson 12/app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/local/TasksDaoTest.kt
@@ -0,0 +1,88 @@
+package com.example.android.architecture.blueprints.todoapp.local
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.local.ToDoDatabase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.hamcrest.CoreMatchers.notNullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+@SmallTest
+class TasksDaoTest {
+
+    // Executes each task synchronously using Architecture Components.
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var database: ToDoDatabase
+
+    @Before
+    fun initDb() {
+        // Using an in-memory database so that the information stored here disappears when the
+        // process is killed.
+        database = Room.inMemoryDatabaseBuilder(
+                getApplicationContext(),
+                ToDoDatabase::class.java
+        ).allowMainThreadQueries().build()
+    }
+
+    @After
+    fun closeDb() = database.close()
+
+    @Test
+    fun insertTaskAndGetById() = runBlockingTest {
+        // GIVEN - Insert a task.
+        val task = Task("title", "description")
+        database.taskDao().insertTask(task)
+
+        // WHEN - Get the task by id from the database.
+        val loaded = database.taskDao().getTaskById(task.id)
+
+        // THEN - The loaded data contains the expected values.
+        assertThat<Task>(loaded as Task, notNullValue())
+        assertThat(loaded.id, `is`(task.id))
+        assertThat(loaded.title, `is`(task.title))
+        assertThat(loaded.description, `is`(task.description))
+        assertThat(loaded.isCompleted, `is`(task.isCompleted))
+    }
+
+    @Test
+    fun updateTaskAndGetById() = runBlockingTest {
+        // 1. Insert a task into the DAO.
+        val task = Task("title", "description")
+        database.taskDao().insertTask(task)
+
+        // 2. Update the task by creating a new task with the same ID but different attributes.
+        val newTask = Task("title1", "description1")
+        val loaded = database.taskDao().getTaskById(task.id)
+        loaded.let { newTask.id = it?.id.toString() }
+
+        database.taskDao().updateTask(newTask)
+        val loadedNewTask = database.taskDao().getTaskById(task.id)
+
+        // 3. Check that when you get the task by its ID, it has the updated values.
+        assertThat<Task>(loaded as Task, notNullValue())
+        assertThat(loaded.id, `is`(task.id))
+        assertThat(loaded.title, `is`(task.title))
+        assertThat(loaded.description, `is`(task.description))
+        assertThat(loaded.isCompleted, `is`(task.isCompleted))
+        assertThat<Task>(loadedNewTask as Task, notNullValue())
+        assertThat(loadedNewTask.id, `is`(newTask.id))
+        assertThat(loadedNewTask.title, `is`(newTask.title))
+        assertThat(loadedNewTask.description, `is`(newTask.description))
+        assertThat(loadedNewTask.isCompleted, `is`(newTask.isCompleted))
+    }
+}
---
+++ Lesson 12/app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,91 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import androidx.lifecycle.ViewModel
+import com.example.android.architecture.blueprints.todoapp.MainCoroutineRule
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.FakeDataSource
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
+import kotlinx.coroutines.Dispatchers
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+internal class StatisticsUtilsTest (
+        private val tasksRepository: TasksRepository
+) : ViewModel(){
+
+    private val task1 = Task("Title1", "Description1")
+    private val task2 = Task("Title2", "Description2")
+    private val task3 = Task("Title3", "Description3")
+    private val remoteTasks = listOf(task1, task2).sortedBy { it.id }
+    private val localTasks = listOf(task3).sortedBy { it.id }
+
+    private lateinit var tasksRemoteDataSource: FakeDataSource
+    private lateinit var tasksLocalDataSource: FakeDataSource
+
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    //No task is completed
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = false)
+        )
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(100f))
+    }
+
+    //All tasks are completed
+    @Test
+    fun getActiveAndCompletedStats_Completed_returnsZeroHundred() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = listOf<Task>(
+                Task("title", "desc", isCompleted = true)
+        )
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertThat(result.completedTasksPercent, `is`(100f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_empty_returnsZeros() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = emptyList<Task>()
+
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_nullError_returnsZeros() {
+
+        // Create an active tasks (the false makes this active)
+        val tasks = null
+
+        // Call our function
+        val result = getActiveAndCompletedStats(tasks)
+
+        // Check the result
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(0f))
+    }
+}
